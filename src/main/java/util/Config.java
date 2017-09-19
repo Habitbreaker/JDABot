e213f7fe0abbb57ac2cf4e632159f1a5ea9bbaeb
habitbreaker <habitbreaker@live.de>
2017-09-19 21:46:57 +0200

    inital commit testbranch

---
+++ src/main/java/util/Config.java
@@ -0,0 +1,32 @@
+package util;
+
+public class Config {
+    public  String TOKEN;
+    public  String PREFIX;
+
+    public String getTOKEN() {
+        return TOKEN;
+    }
+
+    public void setTOKEN(String TOKEN) {
+        this.TOKEN = TOKEN;
+    }
+
+    public String getPREFIX() {
+        return PREFIX;
+    }
+
+    public void setPREFIX(String PREFIX) {
+        this.PREFIX = PREFIX;
+    }
+
+    public String getPATH_FUN_CMDS() {
+        return PATH_FUN_CMDS;
+    }
+
+    public void setPATH_FUN_CMDS(String PATH_FUN_CMDS) {
+        this.PATH_FUN_CMDS = PATH_FUN_CMDS;
+    }
+
+    public  String PATH_FUN_CMDS;
+}
