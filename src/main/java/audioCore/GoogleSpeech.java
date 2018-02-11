fc3c62b51a6b5f748dd823f9c088beb4ca367a43
Habitbreaker <habitbreaker@live.de>
2018-02-11 16:38:04 +0100

    Implemented GoogleSpeech API

--- src/main/java/audioCore/GoogleSpeech.java
+++ src/main/java/audioCore/GoogleSpeech.java
@@ -1,4 +1,32 @@
 package audioCore;
 
+import com.darkprograms.speech.synthesiser.SynthesiserV2;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.StandardCopyOption;
+
 public class GoogleSpeech {
+    private SynthesiserV2 synthesiser = new SynthesiserV2("YOUR API KEY");
+    private String text;
+
+    public GoogleSpeech(String text) {
+        this.text = text;
+    }
+
+
+    public File writeToFile() throws IOException {
+        InputStream initialStream = synthesiser.getMP3Data(this.text);
+        File targetFile = new File("out/tts/tts_file.mp3");
+
+        java.nio.file.Files.copy(
+                initialStream,
+                targetFile.toPath(),
+                StandardCopyOption.REPLACE_EXISTING);
+
+        initialStream.close();
+
+        return targetFile;
+    }
 }
